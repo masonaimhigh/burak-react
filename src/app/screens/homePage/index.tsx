e0f6a9187042f85ee55a425ae03011f6ccde6437
masonaimhigh <mvlogs054@gmail.com>
2024-08-01 15:12:32 +0900

    feat: create homePage Redux configuration

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Statistics from "./Statistics";
 import PopularDishes from "./PopularDishes";
 import NewDishes from "./NewDishes";
@@ -8,6 +8,19 @@ import Events from "./Events";
 import "../../../css/home.css"
 
 export default function HomePage() {
+
+  useEffect(() => {
+    // Selector: Store => Data    #3
+
+    // // Backend server data request => DATA   #1
+    // slice: Data => Redux Store   #2
+
+    // const result = [
+    //   {
+
+
+  }, []);
+
     return <div className={"homepage"}>
       <Statistics/>
       <PopularDishes/>
