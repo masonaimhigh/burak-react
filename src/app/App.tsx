993950ef69561fbedbbf98f612b080a69c330fe5
masonscript4 <heymino68@gmail.com>
2025-07-27 17:36:32 +0900

    fix: modify root index render logic

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import "../css/app.css";
 import { Box, Button, Container, Stack, Typography } from "@mui/material";
-import { RippleBadge } from "./MaterialTheme/styled";
 import { Link, Route, Switch, useLocation } from 'react-router-dom';
 import { HomePage } from "./screens/homePage";
 import { ProductsPage } from './screens/productsPage';
@@ -10,6 +8,9 @@ import { UserPage } from './screens/userPage';
 import { HomeNavbar } from "./components/headers/HomeNavbar";
 import { OtherNavbar } from "./components/headers/OtherNavbar";
 import { Footer } from "./components/footer";
+import "../css/app.css";
+import "../css/navbar.css"
+import { HelpPage } from "./screens/helpPage";
 
  
 function App() {
@@ -26,6 +27,9 @@ function App() {
           </Route>
           <Route path="/member-page">
             <UserPage />
+          </Route>
+          <Route path="/help">
+            <HelpPage />
           </Route>
           <Route path="/">
             <HomePage />
