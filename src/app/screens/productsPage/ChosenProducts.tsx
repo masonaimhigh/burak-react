f9f56cbf579f180c0ac27c2995015f02b4b4a0e7
masonscript4 <heymino68@gmail.com>
2025-08-01 17:57:59 +0900

    feat: publish chosenProduct sectional component

--- src/app/screens/productsPage/ChosenProducts.tsx
+++
@@ -1,3 +0,0 @@
-export default function ChosenProducts () {
-    return <div>ChosenProduct</div>
-  }
--- src/app/screens/productsPage/index.tsx
+++ src/app/screens/productsPage/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Switch, useRouteMatch } from "react-router-dom";
 import { Container } from "@mui/material";
-import ChosenProduct from "./ChosenProducts";
+import ChosenProduct from "./ChosenProduct";
 import Products from "./Products";
 import "../../../css/products.css";
 
